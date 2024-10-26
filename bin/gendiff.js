38fea41d8ab2c07fc5152bc8989ac437deb19ebf
mvv712 <mvv712@mail.ru>
2024-10-26 13:05:37 +0300

    Выполнен второй шаг проекта

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,10 +1,15 @@
 #!/usr/bin/env node
 
-import program from 'commander';
+import { program } from 'commander';
 
 program
  .name('gendiff')
  .description('Compares two configuration files and shows a difference.')
  .version('1.0.0');
+
+program
+ .option('-f, --format [type]', 'output format')
+ .argument('<file1>')
+ .argument('<file2>');
 
 program.parse();
