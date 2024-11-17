64d3740eaf8993bc102f7da1a9eca69adc3c31ff
mvv712 <mvv712@mail.ru>
2024-11-17 18:10:35 +0300

    Исправлены ошибки линтера

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -8,6 +8,8 @@ const markers = {
 
 const getSpaces = (depth, spaces = 4, offset = 2) => ' '.repeat(depth * spaces - offset);
 
+const buildLine = (depth, status, key, value) => `${getSpaces(depth)}${markers[status]} ${key}: ${itemToText(value, depth + 1)}`;
+
 const itemToText = (item, depth) => {
   if (!_.isObject(item)) {
     return `${item}`;
@@ -20,8 +22,6 @@ const itemToText = (item, depth) => {
   return `{\n${items.join('\n')}\n${getSpaces(depth, 4, 4)}}`;
 };
 
-const buildLine = (depth, status, key, value) => `${getSpaces(depth)}${markers[status]} ${key}: ${itemToText(value, depth + 1)}`;
-
 export default (tree) => {
   const iter = (curNode, depth) => {
     const items = curNode
@@ -30,15 +30,16 @@ export default (tree) => {
 
         if (stat === 'nested') {
           return buildLine(depth, 'matched', key, iter(value, depth + 1));
-        } else if (['received', 'expected', 'matched'].includes(stat)) {
+        }
+        if (['received', 'expected', 'matched'].includes(stat)) {
           return buildLine(depth, stat, key, value);
-        } else if (stat === 'exchanged') {
+        }
+        if (stat === 'exchanged') {
           const expected = buildLine(depth, 'expected', key, value.old);
           const received = buildLine(depth, 'received', key, value.new);
           return `${expected}\n${received}`;
-        } else {
-          throw new Error(`Cannot get stat ${stat}`);
         }
+        throw new Error(`Cannot get stat ${stat}`);
       });
 
     return `{\n${items.join('\n')}\n${getSpaces(depth, 4, 4)}}`;
