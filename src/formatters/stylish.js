70e8292c769256e6cff9ca6790f5fd89ffd6fafe
mvv712 <mvv712@mail.ru>
2024-11-17 18:01:48 +0300

    Внесены правки при выводе сравнения в классическом формате

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -9,16 +9,18 @@ const markers = {
 const getSpaces = (depth, spaces = 4, offset = 2) => ' '.repeat(depth * spaces - offset);
 
 const itemToText = (item, depth) => {
-  if (!_.isPlainObject(item)) {
-    return item;
+  if (!_.isObject(item)) {
+    return `${item}`;
   }
 
   const items = Object
     .entries(item)
-    .map(([key, value]) => `${getSpaces(depth)}${markers.matched} ${key}: ${itemToText(value, depth + 1)}`);
+    .map(([key, value]) => buildLine(depth, 'matched', key, value));
 
   return `{\n${items.join('\n')}\n${getSpaces(depth, 4, 4)}}`;
 };
+
+const buildLine = (depth, status, key, value) => `${getSpaces(depth)}${markers[status]} ${key}: ${itemToText(value, depth + 1)}`;
 
 export default (tree) => {
   const iter = (curNode, depth) => {
@@ -27,15 +29,16 @@ export default (tree) => {
         const { stat, key, value } = node;
 
         if (stat === 'nested') {
-          return `${getSpaces(depth)}${markers.matched} ${key}: ${iter(value, depth + 1)}`;
-        } if (['received', 'expected', 'matched'].includes(stat)) {
-          return `${getSpaces(depth)}${markers[stat]} ${key}: ${itemToText(value, depth + 1)}`;
-        } if (stat === 'exchanged') {
-          const expected = `${getSpaces(depth)}${markers.expected} ${key}: ${itemToText(value.old, depth + 1)};`;
-          const received = `${getSpaces(depth)}${markers.received} ${key}: ${itemToText(value.new, depth + 1)}`;
+          return buildLine(depth, 'matched', key, iter(value, depth + 1));
+        } else if (['received', 'expected', 'matched'].includes(stat)) {
+          return buildLine(depth, stat, key, value);
+        } else if (stat === 'exchanged') {
+          const expected = buildLine(depth, 'expected', key, value.old);
+          const received = buildLine(depth, 'received', key, value.new);
           return `${expected}\n${received}`;
+        } else {
+          throw new Error(`Cannot get stat ${stat}`);
         }
-        throw new Error(`Cannot get stat ${stat}`);
       });
 
     return `{\n${items.join('\n')}\n${getSpaces(depth, 4, 4)}}`;
