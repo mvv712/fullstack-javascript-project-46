2cb99698be8b287c022b296423714b21f40e9762
mvv712 <mvv712@mail.ru>
2024-12-01 14:56:34 +0300

    Исправлены ошибки линтера

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -12,7 +12,6 @@ const types = [
 
 const formats = ['stylish', 'plain', 'json'];
 
-
 describe.each(types)('gendiff filepath1.%s filepath2.%s', (type1, type2) => {
   const file1 = getCurrentPath(`file1.${type1}`);
   const file2 = getCurrentPath(`file2.${type2}`);
